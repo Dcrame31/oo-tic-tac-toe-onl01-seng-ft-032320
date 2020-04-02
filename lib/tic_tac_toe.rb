4e6d430eb6abb8a4193ec5c8cc76a048ae4fd2e7
Diane Crame <dcrame@icloud.com>
2020-04-02 21:23:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,8 +12,8 @@ class TicTacToe
     ]
 
   
-  def initialize(board = nil)
-    @board = board || Array.new(9," ")
+  def initialize()
+    @board =  Array.new(9," ")
   end
   
   def display_board
