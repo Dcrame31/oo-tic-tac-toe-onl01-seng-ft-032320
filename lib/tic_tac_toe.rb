d8620659167605ed559e8d67b001ad3aa005bd3a
Diane Crame <dcrame@icloud.com>
2020-04-02 21:05:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,49 +53,91 @@ class TicTacToe
   end
   
   def turn_count
-    turn_count = 0
-    @board.each do |slot|
-      if slot=="X" || slot=="O"
-        turn_count+=1
-      end
-    end
-    turn_count
+    count = 0
+    @board.map {|move| count +=1 if move != " "}
+    count
   end
+  
   def current_player
-    turn_count%2==0 ? "X" : "O"
+    turn_count.even?== true? "X" : "O"
   end
+  
   def won?
-    odp = WIN_COMBINATIONS.detect do |comb|
-      @board[comb[0]]=="X" && @board[comb[1]]=="X" && @board[comb[2]]=="X" || @board[comb[0]]=="O" && @board[comb[1]]=="O" && @board[comb[2]]=="O"
-        end
-  odp ? odp : false
-  end
-  def full?
-    @board.all?{|slot| slot == "X" || slot == "O"}? true : false
-  end
-  def draw?
-    won? ? false : full? ? true : false
-  end
-  def over?
-    won? || draw? || full?
-  end
-  def winner
-    win = won?
-    if(!win)
-      nil
-    else
-      @board[win[0]]
-    end
-  end
-  def play
-    until over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
+   board_index_X = @board.each_index.select {|index| @board[index] == "X"}
+   board_index_O = @board.each_index.select {|index| @board[index] == "O"}
+
+    winning_combinations_x = WIN_COMBINATIONS.map do |index| 
+      index.select {|num| board_index_X.include?(num)}
     end
 
+    winning_combinations_o = WIN_COMBINATIONS.map do |index|
+      index.select {|num| board_index_O.include?(num)}
+    end
+
+    winning_arrays_x = winning_combinations_x.select{|array|array.length > 2}
+    winning_arrays_o = winning_combinations_o.select{|array|array.length > 2}
+    winning_arrays = winning_arrays_o + winning_arrays_x
+    if winning_arrays == []
+      false
+    else winning_arrays.flatten
+    end
   end
+  
+  def full?
+    @board.none? {|s| s == " "}
+  end
+  
+  def draw?
+    if full? && !won?
+      true
+    elsif won?
+      false
+    elsif !full? && !won?
+      false
+    end
+  end
+  
+  def over?
+    true if won? || full?
+  end
+  
+  def winner
+     board_index_X = @board.each_index.select {|index| @board[index] == "X"}
+     board_index_O = @board.each_index.select {|index| @board[index] == "O"}
+
+    winner_X = WIN_COMBINATIONS.map do |index| 
+      index.all? {|num| board_index_X.include?(num)}
+    end
+    
+    winner_O = WIN_COMBINATIONS.map do |index| 
+      index.all? {|num| board_index_O.include?(num)}
+    end
+      x = winner_X.any? {|x| x == true}
+      o =winner_O.any? {|o| o == true} 
+      if x && o == true
+        false
+    elsif
+        o == true
+      "O"
+    elsif 
+        x == true 
+      "X"
+    else
+    end
+  end
+  
+  def play
+    until over? == true
+    turn
+    draw?
+    won?
+    end
+      if won? != false
+        puts "Congratulations #{winner}!"
+      elsif
+        draw? == true
+        puts "Cat's Game!"
+    end
+  end
+  
 end
